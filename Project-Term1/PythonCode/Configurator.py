af9c34fe06192d8cea44064235c34277266d4d1e
Venkatesh Narayana <47418125+VenkateshNarayana@users.noreply.github.com>
2019-04-23 21:11:20 +0530

    Add files via upload

---
+++ Project-Term1/PythonCode/Configurator.py
@@ -0,0 +1,36 @@
+EMAIL = 'tataskypackrecommendersd@gmail.com'
+PASSWORD = 'testpypackage0319$$'
+
+MonthlyNetworkFee = 153.40
+
+TataSkySDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TataSkySDChannelsCompleteList_ver4.xlsx'
+TataSkyHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TataSkyHDChannelsCompleteList_ver4.xlsx'
+
+SunDirectSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_SunDirectSDChannelsCompleteList_ver4.xlsx'
+SunDirectHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_SunDirectyHDChannelsCompleteList_ver4.xlsx'
+
+VideoconSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_VideoconSDChannelsCompleteList_ver4.xlsx'
+VideoconHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_VideoconHDChannelsCompleteList_ver4.xlsx'
+
+AirtelSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_AirtelSDChannelsCompleteList_ver4.xlsx'
+AirtelHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_AirtelHDChannelsCompleteList_ver4.xlsx'
+
+TNArasuCableSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TNArasuCableSDChannelsCompleteList_ver4.xlsx'
+TNArasuCableHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TNArasuCableHDChannelsCompleteList_ver4.xlsx'
+
+TNMutlicityDigConsortiumSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TNMutliDigConSDChannelsCompleteList_ver4.xlsx'
+TNMutlicityDigConsortiumHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_TNMutliDigConHDChannelsCompleteList_ver4.xlsx'
+
+KLPKDYESTVSDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_KLYESTVPKDSD_CompleteList_ver3.xlsx'
+KLPKDYESTVHDPackPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky_Packages/Package_KLYESTVPKDHD_CompleteList_ver3.xlsx'
+
+
+PyPackDataFilesPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky/Datafiles'
+PyPackRecReportPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas/TataSky/RecommendationReport'
+PyPackProgPath = 'C:/Users/God/Documents/Venkatesh/INSAID/Anaconda-SW/NumpyAndPandas'
+
+def MyFunction():
+    '''
+    This is a test function
+    '''
+    print('This is a test function')
